75cc8de632cc8a7001406f8df90a80c093e3d5e7
kagutsuchi57 <103054544+kagutsuchi57@users.noreply.github.com>
2023-09-29 20:43:02 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@ echo "Pulling Changes from github..."
 git pull -f -q
 echo "Installing Requirements..."
 pip install --quiet -U -r requirements.txt
-uvicorn api:app --host=0.0.0.0 --port=${PORT:-5000} & python3 -m MeshRenameBot
+uvicorn api:app --host=0.0.0.0 --port=${PORT:-5000} & python3 -m bot
