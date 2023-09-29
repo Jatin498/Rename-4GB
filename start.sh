85cb50ead76bd7913ca8933b070119d20ab68a9b
kagutsuchi57 <103054544+kagutsuchi57@users.noreply.github.com>
2023-09-29 22:16:59 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,3 @@
-echo "Pulling Changes from github..."
 git pull -f -q
-echo "Installing Requirements..."
 pip install --quiet -U -r requirements.txt
 uvicorn api:app --host=0.0.0.0 --port=${PORT:-5000} & python3 -m bot
