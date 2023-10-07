e08f43ee58306415b34eba5ba16a45ed2eb09835
kagutsuchi57 <103054544+kagutsuchi57@users.noreply.github.com>
2023-10-07 16:41:41 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -148,7 +148,7 @@ async def send_doc(client,message):
        		            total_rename(int(botid),prrename)
        		            total_size(int(botid),prsize,file.file_size)
        		        else:
-       		            uploadlimit(message.from_user.id,214748364800)
+       		            uploadlimit(message.from_user.id, 4147483648)
        		            usertype(message.from_user.id,"Free")
 	
        		            await message.reply_text(f'Your Plane Expired On {buy_date}',quote=True)
@@ -160,7 +160,7 @@ async def send_doc(client,message):
        		    if buy_date:
        		        pre_check = check_expi(buy_date)
        		        if pre_check == False:
-       		            uploadlimit(message.from_user.id,2147483648)
+       		            uploadlimit(message.from_user.id, 4147483648)
        		            usertype(message.from_user.id,"Free")
        		        
        		    filesize = humanize.naturalsize(file.file_size)
